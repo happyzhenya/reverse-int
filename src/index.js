5b1e8fa68540e006eda858ec7d5235f5dca2dd89
happyzhenya <happy.zhenya@gmail.com>
2022-01-08 11:31:01 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let result = 0;
+  if (n<0) { n = Math.abs(n);}
+ 
+  while (n) {
+    result = result * 10 + n % 10;
+    n = Math.floor(n / 10);
+  }
+
+  return result;
 }
